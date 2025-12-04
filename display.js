b1ddbda358561abd7dd54984d1ccbe371bb2b995
Vladisalv Gorea <mrvlad.frosty@gmail.com>
2025-12-04 12:49:34 +0200

    Lagiacrus full working

--- display.js
+++ display.js
@@ -11,11 +11,19 @@ function overview() {
   let o = document.getElementById("overview");
   let m = document.getElementById("moveset");
   let d = document.getElementById("drops");
+  let bW = document.getElementById("weaknesses-button");
+  let bO = document.getElementById("overview-button");
+  let bM = document.getElementById("combat-button");
+  let bD = document.getElementById("materials-button");
   
   if (o.style.display === "block") {
     return false;
   }
   else {
+    bO.className += " current";
+    bW.classList.remove("current");
+    bM.classList.remove("current");
+    bD.classList.remove("current");
     w.style.display = "none";
     o.style.display = "block";
     m.style.display = "none";
@@ -28,11 +36,20 @@ function weaknesses() {
   let o = document.getElementById("overview");
   let m = document.getElementById("moveset");
   let d = document.getElementById("drops");
+  let bW = document.getElementById("weaknesses-button");
+  let bO = document.getElementById("overview-button");
+  let bM = document.getElementById("combat-button");
+  let bD = document.getElementById("materials-button");
   
   if (w.style.display === "flex") {
     return false;
   }
   else {
+    bO.classList.remove("current");
+    bW.className += " current";
+    bM.classList.remove("current");
+    bD.classList.remove("current");
+  
     w.style.display = "flex";
     o.style.display = "none";
     m.style.display = "none";
@@ -44,11 +61,19 @@ function combat() {
   let o = document.getElementById("overview");
   let m = document.getElementById("moveset");
   let d = document.getElementById("drops");
+  let bW = document.getElementById("weaknesses-button");
+  let bO = document.getElementById("overview-button");
+  let bM = document.getElementById("combat-button");
+  let bD = document.getElementById("materials-button");
   
   if (m.style.display === "block") {
     return false;
   }
   else {
+    bO.classList.remove("current");
+    bW.classList.remove("current");
+    bM.className += " current";
+    bD.classList.remove("current");
     w.style.display = "none";
     o.style.display = "none";
     m.style.display = "block";
@@ -61,14 +86,20 @@ function materials() {
   let o = document.getElementById("overview");
   let m = document.getElementById("moveset");
   let d = document.getElementById("drops");
+  let bW = document.getElementById("weaknesses-button");
+  let bO = document.getElementById("overview-button");
+  let bM = document.getElementById("combat-button");
+  let bD = document.getElementById("materials-button");
   
   if (d.style.display === "block") {
     return false;
   }
-  else {
+  bO.classList.remove("current");
+    bW.classList.remove("current");
+    bM.classList.remove("current");
+    bD.className += " current";
     w.style.display = "none";
     o.style.display = "none";
     m.style.display = "none";
     d.style.display = "block";
   }
-}
