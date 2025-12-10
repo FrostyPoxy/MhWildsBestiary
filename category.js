72d656eceb899ac46d276473a786f5421d07ec32
Vladisalv Gorea <mrvlad.frosty@gmail.com>
2025-12-10 21:42:12 +0200

    zoh shia + ajaraka + filter

--- category.js
+++ category.js
@@ -1,5 +1,5 @@
 function myFunction() {
-  var x = document.getElementById("side-bar");
+  let x = document.getElementById("side-bar");
   if (x.style.display === "flex") {
     x.style.display = "none";
   } else {
@@ -7,29 +7,23 @@ function myFunction() {
   }
 }
 
-// function allMonsters() {
-//   let amphibians = document.getElementById("amphibian");
-//   let birdWyvern = document.getElementById("bird-wyvern");
-//   let bruteWyvern = document.getElementById("brute-wyvern");
-//   let cephalopod = document.getElementById("cephalopod");
-//   let construct = document.getElementById("construct");
-//   let demiElder = document.getElementById("demi-elder");
-//   let fangedBeast = document.getElementById("fanged-beast");
-//   let flyingWyvern = document.getElementById("flying-wyvern");
-//   let leviathan = document.getElementById("leviathan");
-//   let temnocerant = document.getElementById("temnocerant");
-  
-//   if (.style.display === "block") {
-//     return false;
-//   }
-//   else {
-//     bO.className += " current";
-//     bW.classList.remove("current");
-//     bM.classList.remove("current");
-//     bD.classList.remove("current");
-//     w.style.display = "none";
-//     o.style.display = "block";
-//     m.style.display = "none";
-//     d.style.display = "none";
-//   }
-// }
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("card");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    x[i].classList.remove("show");
+    if (x[i].className.indexOf(c) > -1) x[i].className += " show";
+  }
+}
+
+var btnContainer = document.getElementById("side-bar");
+var btns = btnContainer.getElementsByClassName("button");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("current");
+    current[0].className = current[0].className.replace(" current", "");
+    this.className += " current";
+  });
+}
